79bbf899bcfd5b5480a2d80ad0b4292a39fd2751
Shalee <shaleew2015@gmail.com>
2018-08-18 16:54:21 -0400

    updated

---
+++ DogWalking/src/app/top-box/top-box.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-top-box',
+  templateUrl: './top-box.component.html',
+  styleUrls: ['./top-box.component.scss']
+})
+export class TopBoxComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
